6f259dd381f0b28bed32fd84dfa3fc9ab2bffc9a
mbargiel <31691087+mabargiel@users.noreply.github.com>
2026-02-12 10:48:44 +0100

    Configure ci and cd (#2)
    * Configure ci and cd
    
    * Fix ci
    
    * Add mini galerry and sections

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,12 +3,17 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
+const dataset =
+  process.env.SANITY_STUDIO_DATASET ||
+  process.env.NEXT_PUBLIC_SANITY_DATASET ||
+  'dev'
+
 export default defineConfig({
   name: 'default',
   title: 'alghero-villa-cms',
 
   projectId: 'x4fnelpw',
-  dataset: 'dev',
+  dataset,
 
   plugins: [structureTool(), visionTool()],
 
